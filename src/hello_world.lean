e92c9f72c175434cfc292f0b4d6e11019de4fb39
barinderbanwait <b.s.banwait86@gmail.com>
2020-05-30 11:41:23 +0100

    Add some documentation

--- src/hello_world.lean
+++ src/hello_world.lean
@@ -1,8 +1,38 @@
+/-
+Copyright (c) 2020 Barinder Singh Banwait. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Barinder Singh Banwait, with lots of help from the good folk on the Leanprover Zulip chat :)
+-/
 import ring_theory.noetherian
 import ring_theory.integral_closure
 import ring_theory.adjoin
 import ring_theory.algebra
 import linear_algebra.basic
+
+/-!
+# Integrally closedness
+
+Description of this stuff.
+
+## Main definitions
+
+Here.
+
+## Main statements
+
+* `this` is that lemma, in the following form:
+  if N is a finitely generated submodule of an ambient R-module M and I is an ideal of R
+  such that N ⊆ IN, then there exists r ∈ 1 + I such that rN = 0.
+
+## References
+
+* [J. Neukirch, *Algebraic Number Theory*][neukirch-ant]
+
+## Tags
+
+Integrally closed
+
+-/
 
 universes u v
 
@@ -61,24 +91,47 @@ begin
   intros r s,
   intro s_non_zero,
   intro H,
-  rw mem_span,
-  intro p,
-  intro p_H,
-  sorry,
+  let p' : ℕ × ℕ := ⟨0, n⟩,
+  have LM : p' ∈ finset.nat.antidiagonal n,
+  {
+    rw finset.nat.mem_antidiagonal,
+    linarith,
+  },
+  rw ← finset.insert_erase LM at H,
+  simp at H,
+  rw hf at H,
+  simp at H,
+  let  B : (R) := ∑ (x : ℕ × ℕ) in (nat.antidiagonal n).erase p', f x.fst * r ^ x.snd * s ^ x.fst,
+  rw B at H,
+  -- have HJ : r ^ n = 0 - ∑ (x : ℕ × ℕ) in (nat.antidiagonal n).erase p', f x.fst * r ^ x.snd * s ^ x.fst,
+  -- {
+  --   rw add_eq_of_eq_sub,
+  -- }
+
+  -- have K : r^n = (range n).sum (λ i, -1 * f (i+1) * r^(n-i-1) * s^(1+i)),
+  -- {sorry,
+  
+  -- },
+  -- rw K,
+  -- rw mem_span,
+  -- intro p,
+  -- intro p_H,
+  -- apply submodule.sum_mem,
+  -- rw my_set at p_H,
+  -- intro c,
+  -- intro C,
+  -- sorry,
 end
-
-
-
-
-  
-  
 
 
 lemma fundamental_theorem_integrally_closedness (R : Type u) (A : Type v) [integral_domain R] 
 [comm_ring A] [algebra R A] (H : fraction_map R A):
   is_integrally_closed R ↔ is_integrally_closed_in R A :=
 begin
-  sorry,
+  split,
+  rw equiv_johan_absolute,
+  intro H,
+
 end
 
 
